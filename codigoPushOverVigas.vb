15aa4518279fdeab68dd43a04f9cc36aad370f32
Isaac Rosales García <isaacrosales1@hotmail.com>
2017-04-23 23:13:41 -0500

    Matriz de fuerzas
    Se anexo la obtencipon de la matriz de fuerzas

--- codigoPushOverVigas.vb
+++ codigoPushOverVigas.vb
@@ -1,25 +1,29 @@
+Option Base 1
+Sub MatrizRigidez()
 Dim numVigas As Integer
-Dim i, j, barra, gdl As Integer
+Dim i, j, barra, numGDL, nodos As Integer
+Dim gdl
 Dim L, EI
 Dim rigidez, pocisionVigas
 Dim matrizGlobal
 Dim apoyo
 Dim contador As Integer
+Dim mFuerzas, celdas(2)
 Sheets("Datos").Activate
-
+'cuenta el número de nodos existente
 [Nodo].Select
-Do Until ActiveCell.Offset(gdl, 0) = ""
-    gdl = gdl + 1
+Do Until ActiveCell.Offset(nodos, 0) = ""
+    nodos = nodos + 1
 Loop
-gdl = gdl - 1
-
-numVigas = gdl - 1
+nodos = nodos - 1
+'crea las matricez del tamaño necesario
+numVigas = nodos - 1
 ReDim EI(numVigas), L(numVigas)
 ReDim rigidezVigas(numVigas, 4, 4)
 ReDim pocisionVigas(numVigas, 4)
 ReDim apoyo(numVigas, 2)
 [elemento].Select
-
+' obtiene los grados de libertad de cada barra
 contador = 1
 For i = 1 To numVigas
     EI(i) = ActiveCell.Offset(i, 2)
@@ -71,37 +75,32 @@ Select Case ActiveCell.Offset(i + 1, 6)
     End If
 
 Next
-
-    
 For i = 1 To numVigas
-      
- 
     rigidezVigas(i, 1, 1) = 12 * EI(i) / L(i) ^ 3
     rigidezVigas(i, 1, 2) = 6 * EI(i) / L(i) ^ 2
     rigidezVigas(i, 1, 3) = -12 * EI(i) / L(i) ^ 3
     rigidezVigas(i, 1, 4) = 6 * EI(i) / L(i) ^ 2
     
-
     rigidezVigas(i, 2, 1) = 6 * EI(i) / L(i) ^ 2
     rigidezVigas(i, 2, 2) = 4 * EI(i) / L(i)
     rigidezVigas(i, 2, 3) = -6 * EI(i) / L(i) ^ 2
     rigidezVigas(i, 2, 4) = 2 * EI(i) / L(i)
-  
-    
 
     rigidezVigas(i, 3, 1) = -12 * EI(i) / L(i) ^ 3
     rigidezVigas(i, 3, 2) = -6 * EI(i) / L(i) ^ 2
     rigidezVigas(i, 3, 3) = 12 * EI(i) / L(i) ^ 3
     rigidezVigas(i, 3, 4) = -6 * EI(i) / L(i) ^ 2
     
-
     rigidezVigas(i, 4, 1) = 6 * EI(i) / L(i) ^ 2
     rigidezVigas(i, 4, 2) = 2 * EI(i) / L(i)
     rigidezVigas(i, 4, 3) = -6 * EI(i) / L(i) ^ 2
     rigidezVigas(i, 4, 4) = 4 * EI(i) / L(i)
 Next
-ReDim matrizGlobal(gdl, gdl)
-
+celdas(1) = 8
+celdas(2) = 3
+gdl = gradosLibertad(celdas, nodos)
+numGDL = UBound(gdl)
+ReDim matrizGlobal(numGDL, numGDL)
 For barra = 1 To numVigas
     For i = 1 To 4
         For j = 1 To 4
@@ -113,6 +112,78 @@ For barra = 1 To numVigas
     Next
 
 Next
+celdas(1) = 8
+celdas(2) = 3
 
-
+mFuerzas = fuerzas(celdas, nodos, numGDL)
 End Sub
+
+Function gradosLibertad(valorCeldas, valorNodos)
+    Dim nodos, i, gdl As Integer
+    Dim X, Y As Integer
+    Dim condicion As String
+    Dim resultado
+    Dim contador As Integer
+    contador = 0
+    nodos = valorNodos
+    X = valorCeldas(1)
+    Y = valorCeldas(2)
+    For i = 1 To nodos
+        condicion = Cells(Y + i, X - 1)
+        Select Case condicion
+            Case ""
+                contador = contador + 2
+            Case "Articulado"
+                contador = contador + 1
+            Case "Empotrado"
+        
+        End Select
+    Next
+    ReDim resultado(contador)
+    contador = 1
+    For i = 1 To nodos
+        condicion = Cells(Y + i, X - 1)
+        Select Case condicion
+            Case ""
+                resultado(contador) = 2 * i - 1
+                resultado(contador + 1) = 2 * i
+                contador = contador + 2
+            Case "Articulado"
+                contador = contador + 1
+                resultado(contador + 1) = 2 * i
+            Case "Empotrado"
+        
+        End Select
+    Next
+    gradosLibertad = resultado
+End Function
+
+Function fuerzas(valorCeldas, valorNodos, valorGDL)
+    Dim nodos, i, gdl As Integer
+    Dim X, Y As Integer
+    Dim condicion As String
+    Dim resultado
+    Dim contador As Integer
+    gdl = valorGDL
+    ReDim resultado(gdl, 1)
+    contador = 1
+    nodos = valorNodos
+    X = valorCeldas(1)
+    Y = valorCeldas(2)
+    For i = 1 To nodos
+        condicion = Cells(Y + i, X - 1)
+        Select Case condicion
+            Case ""
+                resultado(2 * contador - 1, 1) = Cells(Y + i, X)
+                resultado(2 * contador, 1) = Cells(Y + i, X + 1)
+                contador = contador + 1
+            Case "Articulado"
+                resultado(2 * contador, 1) = Cells(Y + i, X + 1)
+                contador = contador + 1
+            Case "Empotrado"
+        
+        End Select
+    Next
+    fuerzas = resultado
+End Function
+
